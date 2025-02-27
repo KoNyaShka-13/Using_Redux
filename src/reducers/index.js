45e9e596677e395629b828ceda6d137e0443df31
KoNyaShKa <ggiziawpgg@gmail.com>
2025-02-27 11:06:09 +0300

    update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,14 @@
 const initialState = {
     heroes: [],
-    heroesLoadingStatus: 'idle',//Статус загрузки героев (по умолчанию 'idle' — бездействие)
-    filters: []
+    heroesLoadingStatus: 'idle',// Статус загрузки героев (по умолчанию 'idle' — бездействие)
+    filters: [],
+    filtersLoadingStatus: 'idle',// Статус загрузки фильтров
+    activeFilter: 'all',// Активный фильтр (по умолчанию 'all' — все герои)
+    filteredHeroes: []//Отфильтровал список
 }
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
+    switch (action.type) {//Если action.type не соответствует ни одному из кейсов, возвращается текущее состояние (state).
         case 'HEROES_FETCHING':
             return {
                 ...state,
@@ -14,13 +17,64 @@ const reducer = (state = initialState, action) => {
         case 'HEROES_FETCHED':
             return {
                 ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
+                heroes: action.payload,//heroes обновляется данными из action.payload (новый список героев)
+                
+                filteredHeroes: state.activeFilter === 'all' ? 
+                                action.payload : 
+                                action.payload.filter(item => item.element === state.activeFilter),//Если активный фильтр 'all', то filteredHeroes равен всему списку героев. Иначе фильтруются герои, у которых element совпадает с activeFilter.
+                heroesLoadingStatus: 'idle'//Снова бездейтвие 
             }
         case 'HEROES_FETCHING_ERROR':
             return {
                 ...state,
                 heroesLoadingStatus: 'error'
+            }
+        case 'FILTERS_FETCHING':
+            return {
+                ...state,
+                filtersLoadingStatus: 'loading'
+            }
+        case 'FILTERS_FETCHED':
+            return {
+                ...state,
+                filters: action.payload,//filters обновляется данными из action.payload (новый список фильтров)
+                filtersLoadingStatus: 'idle'
+            }
+        case 'FILTERS_FETCHING_ERROR':
+            return {
+                ...state,
+                filtersLoadingStatus: 'error'
+            }
+        case 'ACTIVE_FILTER_CHANGED':
+            return {
+                ...state,
+                activeFilter: action.payload,
+                filteredHeroes: action.payload === 'all' ? //filteredHeroes обновляется в зависимости от нового активного фильтра: Если фильтр 'all', то filteredHeroes равен всему списку героев.Иначе фильтруются герои, у которых element совпадает с новым активным фильтром.
+                                state.heroes :
+                                state.heroes.filter(item => item.element === action.payload)
+            }
+        
+        case 'HERO_CREATED':
+            //Новый массив    
+            let newCreatedHeroList = [...state.heroes, action.payload];
+            return {
+                ...state,
+                heroes: newCreatedHeroList,
+                // Фильтруем новые данные по фильтру, который сейчас применяется
+                filteredHeroes: state.activeFilter === 'all' ? 
+                                newCreatedHeroList : 
+                                newCreatedHeroList.filter(item => item.element === state.activeFilter)
+            }
+        case 'HERO_DELETED': 
+            //Новый массив
+            const newHeroList = state.heroes.filter(item => item.id !== action.payload);
+            return {
+                ...state,
+                heroes: newHeroList,
+                // Фильтруем новые данные по фильтру, который сейчас применяется
+                filteredHeroes: state.activeFilter === 'all' ? 
+                                newHeroList : 
+                                newHeroList.filter(item => item.element === state.activeFilter)
             }
         default: return state
     }
