93952abcb614e20a758f9b53a455848f5dc120ce
KoNyaShKa <ggiziawpgg@gmail.com>
2025-03-15 09:07:27 +0300

    store enhancers
    string into object

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,24 @@ import { createStore, combineReducers } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore( combineReducers({heroes, filters}),//Объединили 2 редьюсера 
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const enhancer = (createStore) => (...args) => {//Преобразуем, чтобы экшены становились объектами
+     const store = createStore(...args);
+
+     const oldDispatch = store.dispatch;
+     store.dispatch = (action) => {
+          if (typeof action === 'string') {//Проверка условия, если экшен является строкой, то мы в ручную изменяем его
+               return oldDispatch({//Другими словами, в старый диспэтч, что достали, вставляем данные экшена, который является строкой
+                    type: action
+               })
+          }
+          return oldDispatch(action)//Если экшен не является строкой, то кидаем просто экшен в диспэтч
+     }
+
+      return store;
+}
+
+const store = createStore( combineReducers({heroes, filters}), enhancer);//Объединили 2 редьюсера 
+     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+     
 
 export default store;
